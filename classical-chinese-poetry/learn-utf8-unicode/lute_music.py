0309b92257db02f62a3fa137b38161a678ed0fdc
art <beijingbayou@gmail.com>
2019-09-17 12:04:10 -0700

    display using markdown table

--- classical-chinese-poetry/learn-utf8-unicode/lute_music.py
+++ classical-chinese-poetry/learn-utf8-unicode/lute_music.py
@@ -2,16 +2,21 @@ from utf8unicode import Unicode, utf8_unicode_set
 
 if __name__ == '__main__':
     poem="""
-# 彈琴   Lute Playing
-### 劉長卿 Liu Changqing
+### 彈琴   Lute Playing
+##### 劉長卿 Liu Changqing
 
-泠泠七弦上 🎻
-靜聽松風寒 🌲
-古調雖自愛 🎶
-今人多不彈 😞
+> 泠泠七弦上 🎻
+> 靜聽松風寒 🌲
+> 古調雖自愛 🎶
+> 今人多不彈 😞
+
+
 """
-    print(poem)
-    poem_unicode_set = sorted(utf8_unicode_set(poem, {'\n', ' ', '#', '*'}))
 
+    print(poem)
+    poem_unicode_set = sorted(utf8_unicode_set(poem, {'\n', ' ', '#', '*', '>'}))
+
+    print("| Unicode Character | UTF-16 |")
+    print("|:-----------------:|:------:|")
     for c, u in poem_unicode_set:
-        print(f"**{c}** {u}  ")
+        print(f"| **{c}** | {u} |")
