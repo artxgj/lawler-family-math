3f4f102535ce82b0768938c572c492959a69c6bd
art <beijingbayou@gmail.com>
2019-10-20 03:33:34 -0700

    added poems_common.py and changed poems_ngrams.py to poems_ngrams_frequency.py

---
+++ classical_chinese_poetry/poems_common.py
@@ -0,0 +1,26 @@
+import csv
+import io
+from typing import Generator, Tuple
+from p3lib.ngrams import ngrams
+
+attr_poet = 'poet'
+attr_title = 'title'
+attr_poem = 'poem'
+verse_delim = '\n'
+
+poem_fieldnames = [attr_title, attr_poet, attr_poem]
+
+
+def lines_from_poems_csv(poems_csv_path: str) -> Generator[dict, None, None]:
+    with io.open(poems_csv_path, 'r', encoding='utf-8') as istream:
+        rdr = csv.DictReader(istream)
+        for row in rdr:
+            yield row
+
+
+def poems_verse_ngrams(poems: Generator[dict, None, None], ngram_size: int) -> Generator[Tuple[str, ...], None, None]:
+    for poem in poems:
+        for verse in poem['poem'].split(verse_delim):
+            for ngram in ngrams(verse, ngram_size):
+                yield ngram
+
---
+++ classical_chinese_poetry/poems_ngrams_frequency.py
@@ -0,0 +1,54 @@
+import argparse
+import csv
+import io
+
+from p3lib.ngrams import ngrams
+from poems_common import lines_from_poems_csv, poems_verse_ngrams
+
+"""
+def poem_ngrams(csv_poems_filepath: str, output_filepath: str, ngram_size: int) -> None:
+    with open(csv_poems_filepath, 'r') as csv_in:
+        reader = csv.DictReader(csv_in)
+        with open(output_filepath, 'w') as outf:
+            for row in reader:
+                for line in row['poem'].split('+'):
+                    for ngram in ngrams(line, ngram_size):
+                        out = ''.join(list(ngram))
+                        outf.write(f"{out}\n")
+"""
+
+
+def ngram_frequency(poems_csv_path: str, ngram_size: int) -> dict:
+    frequency = {}
+
+    poems = lines_from_poems_csv(poems_csv_path)
+
+    for ngram in poems_verse_ngrams(poems, ngram_size):
+        key = ''.join(ngram)
+        if key not in frequency:
+            frequency[key] = 1
+        else:
+            frequency[key] += 1
+
+    return frequency
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--ngram-size', help='The size of the ngram', type=int, required=True)
+    parser.add_argument('-o', '--output-file', help='The output file of ngrams', required=True)
+    parser.add_argument('-c', '--csv-poems', help='The poems csv file', required=True)
+
+    args = parser.parse_args()
+    freqtable = ngram_frequency(args.csv_poems, args.ngram_size)
+    ordered_freq = sorted(freqtable.items(), key=lambda kv: kv[1], reverse=True)
+
+    with io.open(args.output_file, 'w', encoding='utf-8') as ostream:
+        wrtr = csv.DictWriter(ostream, fieldnames=['bigram', 'count'], lineterminator='\n', quoting=csv.QUOTE_ALL)
+        wrtr.writeheader()
+        for ngram, count in ordered_freq:
+            wrtr.writerow({'bigram': ngram, 'count': count})
+
+
+if __name__ == '__main__':
+    main()
