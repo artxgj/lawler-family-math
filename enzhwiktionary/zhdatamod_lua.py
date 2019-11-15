8e48505d3823fe5db721f3c28cbb6498c9dcd4ab
art <beijingbayou@gmail.com>
2019-11-15 00:31:50 -0800

    convert dial-syn's lua-style associative arrays to lists

--- enzhwiktionary/zhdatamod_lua.py
+++ enzhwiktionary/zhdatamod_lua.py
@@ -11,13 +11,27 @@ class ZhDataModLuaChunk(ABC):
     def content(self, lua_varname: str):
         pass
 
+    def _associate_array_to_list(self, contents: dict):
+        """
+        Lua does not support arrays and uses associative arrays. This method
+        converts the python-version of the associative array to a list
+        :return:
+        """
+        new_contents = {}
+        for key, value in contents.items():
+            if type(value) == dict:
+                new_contents[key] = [value[index] for index in range(1, len(value)+1) if value[index] != ""]
+            else:
+                new_contents[key] = contents[key]
+        return new_contents
+
 
 class ZhDataModTableAssignName(ZhDataModLuaChunk):
     def __init__(self, chunk: str):
         super().__init__(chunk)
 
     def content(self, lua_var: str):
-        return self._rhs.assign_name(lua_var)
+        return self._associate_array_to_list(self._rhs.assign_name(lua_var))
 
 
 class ZhDataModTableAssignIndex(ZhDataModLuaChunk):
@@ -26,7 +40,7 @@ class ZhDataModTableAssignIndex(ZhDataModLuaChunk):
 
     def content(self, lua_var: str):
         table_name, index = lua_var.split('.')
-        return self._rhs.assign_index(index, table_name)
+        return self._associate_array_to_list(self._rhs.assign_index(index, table_name))
 
 
 class ZhDataModTableReturn(ZhDataModLuaChunk):
@@ -34,4 +48,4 @@ class ZhDataModTableReturn(ZhDataModLuaChunk):
         super().__init__(chunk)
 
     def content(self, lua_var: str = None):
-        return self._rhs.return_exp()
+        return self._associate_array_to_list(self._rhs.return_exp())
